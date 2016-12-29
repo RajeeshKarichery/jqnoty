9bd06cd99336ad8c28dae3e5794a7ff5514881c9
RajeeshKarichery <rejeeshmangad@gmail.com>
2016-12-29 17:46:58 +0530

    added popup

--- popup/MyPop.js
+++ popup/MyPop.js
@@ -1,200 +1,154 @@
-/*;( function( window ) {
+;( function( window ) {
 
-  'use strict';
- 
-	function extend( a, b ) {
-		for( var key in b ) { 
-		  if( b.hasOwnProperty( key ) ) {
-			a[key] = b[key];
-		  }
-		}
-		return a;
-	}  	
-	
-	var defaults = {
-             title:'MyTitle',	
-			 template  : '<h1>Hello<h1>',
-			 callback    : {
-				beforeShow  : function () {
-				},
-				onShow      : function () {
-				},
-				afterShow   : function () {
-				},
-				onClose     : function () {
-				},
-				afterClose  : function () {
-				},
-				onCloseClick: function () {
+	'use strict';
+	 	  		 		  
+		$.pops         = {};
+		$.pops.defaults = {
+			size     : 'modal-md',
+			layout	 : 'vertical',
+			title    : 'Title',  	
+			template    : '<h3>Body</h3>',  
+			callback    : {
+						beforeShow  : null,
+						onShow      : null,
+						afterShow   : null,
+						onClose     : null,
+						afterClose  : null,
+						onCloseClick: null,
+						onSubmitClick: null
+					  },
+			buttons:{				
+				save:{
+					isShow:false,
+					type:'submit',
+					text:'Save',
+					addClass:'primary'
 				}
-			  }			 
-        };    
-	
-	 
-	  function SimplePopUp( options ) { 			
-			extend( defaults, options );	
-			
-			
-			var mypop = $("#mypop");
-			var element = '<div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">'+defaults.title+'</h4></div>	<div class="modal-body">'+defaults.template+'</div><div class="modal-footer"><button id="btnClose" type="button" class="btn btn-default" >Close</button></div></div></div></div>';
-					
-			
-			$(mypop).append(element);
-			$('#myModal').on('show.bs.modal', function () {				
-				if(defaults.callback.beforeShow != null){
-					var _response = Object();					
-					return defaults.callback.beforeShow.call(this,_response);
-				}				
-			});
-			
-			$('#myModal').on('hide.bs.modal', function () {
-				if(defaults.callback.onClose != null){
-					var _response = Object();					
-					return defaults.callback.onClose.call(this,_response);
-				}
-			});
-			
-			$('#btnClose').on('click', function () {
-				if(defaults.callback.onCloseClick != null){
-					var _response = Object();					
-					return defaults.callback.onCloseClick.call(this,_response);
-				}
-				else							
-					$.fn.close();
-			});
-			$.fn.open();		   		
-	  } 
-	  
-	  $.fn.open = function() {
-		   $('#myModal').modal('show');
-	  }	  
-	  $.fn.close = function() {
-		   $('#myModal').modal('hide');
-	  }	   
-	  window.SimplePopUp = SimplePopUp;
-	  
-})( window );*/
-
-
-!function(root, factory) {
-	 if (typeof define === 'function' && define.amd) {
-		 define(['jquery'], factory);
-	 } else if (typeof exports === 'object') {
-		 module.exports = factory(require('jquery'));
-	 } else {
-		 factory(root.jQuery);
-	 }
-}(this, function($) {
-
-
-if (typeof Object.create !== 'function') {
-  Object.create = function (o) {
-    function F() {
-    }
-
-    F.prototype = o;
-    return new F();
-  };
-}
-
-
-$.rknoty         = {};
-$.rknoty.returns = 'object';
-$.rknoty.store   = {};
-
-$.rknoty.defaults = {
-  text        : '',
-  template    : '<div class="alert "><span class="rknoty_text"></span> <button type="button" class="close" >&times;</button> </div>',
-  callback:function(){}
-  /*callback    : {
-				beforeShow  : function () {
-				},
-				onShow      : function () {
-				},
-				afterShow   : function () {
-				},
-				onClose     : function () {
-				},
-				afterClose  : function () {
-				},
-				onCloseClick: function () {
-				}
-			  }*/		
-};
-
-
-
-
-var rknotyObject = {
-  init: function (options) {
-	this.options = $.extend({}, $.rknoty.defaults, options);
-	var mypop = $("#mypop");
-			var element = '<div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title"></h4></div>	<div class="modal-body"></div><div class="modal-footer"><button id="btnClose" type="button" class="btn btn-default" >Close</button></div></div></div></div>';				
-			
-			$(mypop).append(element);			
-	
-		
-		$('#btnClose').on('click', function () {
-			
-			if($.rknoty.defaults.callback != null){
-				var _response = Object();					
-				return $.rknoty.defaults.callback.apply(this);
 			}
-			else							
-				$.rknotyRenderer.close();
-		});		
-			
-  }, 
-  show: function () { 
-  }, // end show  
-  close: function () {
-  }, // end close 
+				
+		};
+		var popsObject = {
+			init: function (options) {
+				this.options = $.extend({}, $.pops.defaults, options);
+				this._build();
+				return this;
+			},
+			_build: function () {	
+				var size  = this.options.size;
+				console.log(size);
+		  
+				var element = '<span class="pops_replace">';
+				element	   += '<div class="modal fade" id="popsModal" role="dialog"  tabindex="-1" >';
+				element    += '<div class="modal-dialog '+this.options.size+' "><div class="modal-content">';
+				element    += '<div class="modal-header">';
+				element    += '<button type="button" id="btn-x-close" class="close" data-dismiss="modal">&times;</button>';
+				element    += '<h4 class="modal-title"><span class="pops_text"></span></h4>';
+				element    += '</div>';
+				element    += '<form id="frm-pops" class="form-'+this.options.layout+'">';
+				element    += '<div class="modal-body">';				
+					element    += '<span class="pops_body"></span>';				 				
+				element    += '</div>';
+				element    += '<div class="modal-footer">';
+					var btn_class,btn_type;
+					if(this.options.buttons){
+						if(this.options.buttons.save.isShow){
+							btn_class ='primary';
+							btn_type ='submit';							
+							if(this.options.buttons.save.addClass)
+								btn_class = this.options.buttons.save.addClass;
+							if(this.options.buttons.save.type)
+								btn_type = this.options.buttons.save.type;
+							element += '<button id="btnSave" type="'+btn_type+'" class="btn btn-'+btn_class+'" >'+this.options.buttons.save.text+'</button>';
+						}					
+					}
+					
+				element  += '<button id="btnClose" type="button" class="btn btn-default" >Close</button>';
+				element    += '</div></form></div></div></div>';
+				element    += '</span>';
+				
+				var $popUp = $(element);
+				$popUp.find('.pops_text').html(this.options.title);
+				$popUp.find('.pops_body').html(this.options.template);
+				this.$popUp = $popUp;						
+				if($("span").hasClass("pops_replace")){
+					$('span').remove('.pops_replace');
+				}
+				$('body').append($popUp);
+								
+				var self = this;							
+				$('#btnClose').on('click', function (evt) {				
+					if(self.options.callback.onCloseClick){				
+						return self.options.callback.onCloseClick.apply(self); 							
+					}
+					else	
+						self.close();
+				});
+				
+				$('#btn-x-close').on('click', function (evt) {			
+					if(self.options.callback.onClose){
+						evt.stopPropagation();
+						return self.options.callback.onClose.apply(self);
+					}
+					else
+						self.close();
+				});				
+							
+				if(self.options.callback.beforeShow)
+				  self.options.callback.beforeShow.apply(self);	
+				if(self.options.callback.onShow)
+				  self.options.callback.onShow.apply(self);	  
+				if(self.options.callback.afterShow)
+				  self.options.callback.afterShow.apply(self);	
+				if(self.options.callback.afterClose)
+				  self.options.callback.afterClose.apply(self);				
+				  
+				  
+				$('#frm-pops').on('submit', function (evt) {	
+					evt.preventDefault();
+					var _reponse = Object();
+					var data = JSON.stringify( $('frm-pops').serializeArray() ); 
+					_reponse.formJson = data;						
+					self.options.callback.onSubmitClick.call(self,_reponse);	
+					
+				}); 
+				if(this.options.buttons && this.options.buttons.save && this.options.buttons.save.type){
+					$('#btnSave').on('click', function (evt) {							
+						var _reponse = Object();						
+						_reponse.formJson = JSON.stringify( $('frm-pops').serializeArray() ); ;						
+						self.options.callback.onSubmitClick.call(self,_reponse);	
+						
+					});  	
+				}			
+				 
+			},
+			show: function () {
+				//$('#popsModal').modal('show');				
+				$('#popsModal').modal({
+					backdrop: 'static',
+					keyboard: false
+				});
+				this.shown   = true; 
+				return this;
+			},
+			close: function () {	
+				this.closed   = true;
+				$('#popsModal').modal('hide'); 
+			},
+			closed : false,
+			shown  : false	
+		}; //end popsObject
+		
+		
+		$.popRenderer = {};
+		$.popRenderer.init = function (options) {
+		   var _pop = Object.create(popsObject).init(options);
+		   return _pop; 
+		};				  
+		function mPopUp(options){			
+			return $.popRenderer.init(options);
+		}		  
+		window.mPopUp = mPopUp;
+	  
+})( window );
 
-  setText: function (text) {    
-  },
 
-  stopPropagation: function (evt) {
-    evt = evt || window.event;
-    if (typeof evt.stopPropagation !== "undefined") {
-      evt.stopPropagation();
-    }
-    else {
-      evt.cancelBubble = true;
-    }
-  },
-  closed : false,
-  showing: false,
-  shown  : false
-
-}; // end NotyObject
-
-$.rknotyRenderer = {};
-
-$.rknotyRenderer.init = function (options) {
-   var notification = Object.create(rknotyObject).init(options);
-   $.rknotyRenderer.show(notification);
-};
-
-$.rknotyRenderer.show = function (notification) {
-	$('#myModal').modal('show');	 
-};
-
-$.rknotyRenderer.close = function () {	
-	$('#myModal').modal('hide');	 
-};
-
-
-
-
-			
-
-
-// Helpers
-window.rknoty = function rknoty(options) {
-  return $.rknotyRenderer.init(options);
-};
-
-
-return window.rknoty;
-
-});
